f0831b49025007b377e770a301d8305f87dcd31c
takaoka.daisuke <takaoka.daisuke@moneyforward.co.jp>
2025-10-13 10:57:47 +0900

    remove cache

--- packages/docusaurus-plugin-openapi-docs/lib/openapi/createSchemaExample.d.ts
+++
@@ -1,6 +0,0 @@
-import { SchemaObject } from "./types";
-export type ExampleType = "request" | "response";
-export interface ExampleContext {
-    type: ExampleType;
-}
-export declare const sampleFromSchema: (schema: SchemaObject | undefined, context: ExampleContext) => any;
